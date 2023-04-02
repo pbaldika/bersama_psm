a44833591d9b130f1ba3f3b1d628274df5de0911
Aldika Prya Bagus Ananta <68334626+pbaldika@users.noreply.github.com>
2023-04-02 15:12:23 +0800

    initial commit to start development

---
+++ resources/views/partials/frontend/header.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>HeroBiz<span>.</span></h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+      @include('partials.frontend.navbar')
+      </nav><!-- .navbar -->
+
+      <a class="btn-getstarted scrollto" href="index.html#about">Get Started</a>
+
+    </div>
