8c43b8754084d26cddaf80e3c008e0c83d3ff2ae
Aldika Prya Bagus Ananta <68334626+pbaldika@users.noreply.github.com>
2023-04-08 00:43:34 +0800

    Laravel ui - auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,12 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'telephone',
+        'gender',
+        'address',
+        'dob',
         'password',
+        'role',
     ];
 
     /**
